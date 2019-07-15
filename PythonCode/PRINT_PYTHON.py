8bcb957b630d4ebe98144e3a94bc92abc23427e2
KrishnanshuDey34 <krishnanshudey2014@gmail.com>
2019-07-16 01:12:25 +0530

    Add files via upload

---
+++ PythonCode/PRINT_PYTHON.py
@@ -0,0 +1,4 @@
+print("PPPPP  Y     Y TTTTTTT H   H   OOOO   N     N")
+print(" P   P  Y   Y     T    H---H  O    O  N \   N")
+print(" PPPP     Y       T    H---H  O    O  N   \ N")
+print(" P        Y       T    H   H   OOOO   N     N")
